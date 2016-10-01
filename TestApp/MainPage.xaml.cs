56aa14ae97e6bd72db0746a24db6e7baf860445e
Sindre Rynning Hansen <sindrerhansen@gmail.com>
2016-10-01 09:55:00 +0200

    Adding soket io

--- TestApp/MainPage.xaml.cs
+++ TestApp/MainPage.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +10,19 @@ namespace TestApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        int teller = 0;
+        MainViewModel mvm = new MainViewModel();
         public MainPage()
         {
             this.InitializeComponent();
+            DataContext = mvm;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            button.Content = "I am pressed "+teller.ToString()+" times";
+            teller++;
+            mvm.Tekst = teller.ToString();
         }
     }
 }
---
+++ TestApp/MainViewModel.cs
@@ -0,0 +1,72 @@
+﻿using GalaSoft.MvvmLight;
+using Microsoft.AspNet.SignalR.Client;
+using Newtonsoft.Json.Linq;
+using Quobject.SocketIoClientDotNet.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+
+namespace TestApp
+{
+    public class MainViewModel : ViewModelBase
+
+    {
+        private string tekst = "";
+        public string Tekst {
+            get {
+                return tekst;
+            }
+            set {
+                tekst = value;
+                RaisePropertyChanged("Tekst");
+            }
+        }
+        private readonly CoreDispatcher _dispatcher;
+        public MainViewModel()
+        {
+            var socket = IO.Socket("http://192.168.3.80/");
+            socket.Connect();
+            // whenever the server emits "login", print the login message
+            socket.On("tempUpdate", data => {
+                
+
+                // get the json data from the server message
+                var jobject = data as JToken;
+
+                // get the number of users
+                var numUsers = jobject.Value<int>("numUsers");
+                Tekst = numUsers.ToString();
+                // display the welcome message...
+            });
+
+            Tekst = "Start";
+            _dispatcher = CoreWindow.GetForCurrentThread().Dispatcher;
+
+            var connection = new HubConnection("http://internetuptime.azurewebsites.net/signalr");
+
+            var hubProxy = connection.CreateHubProxy("UptimeHub");
+            hubProxy.On<string>("internetUpTime", UpdateTime);
+
+            connection.Start();
+        }
+
+        private string uptime ="";
+        public string Uptime {
+            get {
+                return uptime;
+            }
+            set {
+                uptime = value;
+                RaisePropertyChanged("Uptime");
+            }
+        }
+
+        private async void UpdateTime(string s)
+        {
+            await _dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => Uptime = s);
+        }
+    }
+}
